43d10135143d8c29dc1987603cf88d01db197993
“rhunter27” <ravenhunter27@gmail.com>
2025-04-17 19:38:12 -0400

    Add User model

---
+++ THE-SOCIAL-NETWORK/src/models/User.ts
@@ -0,0 +1,49 @@
+import { Schema, model, Document, Model, Types } from 'mongoose';
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  thoughts: Types.ObjectId[];
+  friends: Types.ObjectId[];
+  readonly friendCount: number;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username is required'],
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters long'],
+    maxlength: [30, 'Username cannot exceed 30 characters']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address']
+  },
+  thoughts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+  }],
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  toJSON: {
+    virtuals: true
+  },
+  id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model<IUser>('User', UserSchema);
+
+export default User;
